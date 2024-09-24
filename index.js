b6d2b98aef3ad16278eaba8fb7dba53f46a8156b
VuBaoKhanh <153911764+vubaokhannh@users.noreply.github.com>
2024-09-24 12:23:16 +0700

    new

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+app.use(morgan("combined"));
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`);
+    console.log(`Example app listening on port http://loclhost:${port}`);
 });
